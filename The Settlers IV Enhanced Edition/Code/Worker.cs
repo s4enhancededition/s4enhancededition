1cc400a8d795c5ee201dd6ec3f562648c33896c9
TheSettlersIVEnhancedEdition <80218322+TheSettlersIVEnhancedEdition@users.noreply.github.com>
2021-03-12 21:21:02 +0100

    CleanUp

--- The Settlers IV Enhanced Edition/Code/Worker.cs
+++ The Settlers IV Enhanced Edition/Code/Worker.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace S4EE
+﻿namespace S4EE
 {
     class Worker
     {
--- The Settlers IV Enhanced Edition/Pages/AppSettings.xaml.cs
+++ The Settlers IV Enhanced Edition/Pages/AppSettings.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Threading;
