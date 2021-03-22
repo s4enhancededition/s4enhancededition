3afada503210e76553ca78fbd158416dbbb65380
TheSettlersIVEnhancedEdition <80218322+TheSettlersIVEnhancedEdition@users.noreply.github.com>
2021-03-22 20:17:56 +0100

    1.1.5.1090

--- The Settlers IV Enhanced Edition/Windows/Info.xaml.cs
+++ The Settlers IV Enhanced Edition/Windows/Info.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace S4EE.Windows
 {
@@ -22,6 +12,7 @@ namespace S4EE.Windows
         public Info()
         {
             InitializeComponent();
+            Infogrid.Source = new BitmapImage(new Uri(@"/Resources/App_Info_Hotkeys_" + Properties.Settings.Default.Language + @".png", UriKind.RelativeOrAbsolute));
         }
     }
 }
